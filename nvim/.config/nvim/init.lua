a1263510243930c008c5c9d3c03773ba7d586ad4
nirimatt <matt_lucero@outlook.com>
2025-12-18 16:19:58 -0600

    add deno lsp support

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,13 +10,18 @@ vim.keymap.set("n", "<C-PageUp>", ":tabprevious<CR>", { desc = "Previous tab" })
 -- Map Ctrl + PgDn to go to the next tab
 vim.keymap.set("n", "<C-PageDown>", ":tabnext<CR>", { desc = "Next tab" })
 
-vim.lsp.config("denols", {
-  on_attach = on_attach,
-  root_markers = { "deno.json", "deno.jsonc" },
-})
-
-vim.lsp.config("ts_ls", {
-  on_attach = on_attach,
-  root_markers = { "package.json" },
-  single_file_support = false,
-})
+-- deno lsp
+-- vim.g.markdown_fenced_languages = {
+--   "ts=typescript",
+-- }
+-- vim.lsp.enable("denols")
+-- vim.lsp.config("denols", {
+--   on_attach = on_attach,
+--   root_markers = { "deno.json", "deno.jsonc" },
+-- })
+--
+-- vim.lsp.config("ts_ls", {
+--   on_attach = on_attach,
+--   root_markers = { "package.json" },
+--   single_file_support = false,
+-- })
